f7ea016f149d97b398b5d90420e800cbef902192
Jihong Park <ggikko@hotmail.com>
2016-05-08 12:13:18 +0900

    init

---
+++ MyApplication/app/src/main/java/ggikko/me/steppertest/MainActivity.java
@@ -0,0 +1,18 @@
+package ggikko.me.steppertest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * stepper test
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
